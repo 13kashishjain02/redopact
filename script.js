63b22b0c8cbb4a9f819a42a598814a0ad5f8d3de
Rashmi Thakur <rashmi.lata151@gmail.com>
2021-06-30 20:02:25 +0530

    updated

--- script.js
+++ script.js
@@ -14,21 +14,4 @@ function removeTag(){
     });
 }
 
-
-//nav-bar
- 
-var prevScrollpos = window.pageYOffset;
-
-window.onscroll = function(){
-    var currentScrollPos = window.pageYOffset;
-    if (prevScrollpos > currentScrollPos){
-        document.getElementById('header').style.top = "0";
-    }
-    else{
-        document.getElementById('header').style.top = "-100px";
-    }
-
-    prevScrollpos = currentScrollPos;
-}
-
-
+//header
